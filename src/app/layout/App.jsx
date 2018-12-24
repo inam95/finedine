8922248a646c3da167f2956eb5ad696dc3010940
inam95 <mfihassan99@gmail.com>
2018-12-25 00:20:18 +0530

    routing added

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
 
-import RestaurantDashboard from "../../features/restaurant/RestaurantDashboard/RestaurantDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import Home from "../../features/home/Home";
+import RestaurantDashboard from "../../features/restaurant/RestaurantDashboard/RestaurantDashboard";
+import RestaurantDetailedPage from "../../features/restaurant/RestaurantDetailed/RestaurantDetailedPage";
+import UserDetailedPage from "../../features/user/UserDetailedPage/UserDetailedPage";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <RestaurantDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/restaurants" component={RestaurantDashboard} />
+                  <Route
+                    path="/restaurant/:id"
+                    component={RestaurantDetailedPage}
+                  />
+                  <Route path="/profile" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,38 +1,64 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+
+import SignedOutMenu from "./Menus/SignedOutMenu";
+import SignedInMenu from "./Menus/SignedInMenu";
 
 import "./NavBar.css";
 
 class NavBar extends Component {
+  state = {
+    authenticate: false
+  };
+
+  handleSignIn = () => {
+    this.setState({
+      authenticate: true
+    });
+  };
+
+  handleSignOut = () => {
+    this.setState({
+      authenticate: false
+    });
+    this.props.history.push("/");
+  };
+
   render() {
+    const { authenticate } = this.state;
+
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={Link} to="/" header>
             <img
-              src="assets/logo2.png"
+              src="/assets/logo2.png"
               alt="logo"
               style={{ height: "3.5rem", width: "3.5rem" }}
             />
             <h2 className="brand">FineDine</h2>
           </Menu.Item>
-          <Menu.Item name="Events" />
+          <Menu.Item as={NavLink} to="/restaurants" name="Events" />
           <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
+            {authenticate && (
+              <Button
+                floated="right"
+                positive
+                inverted
+                content="Create Event"
+              />
+            )}
           </Menu.Item>
-          <Menu.Item position="right">
-            <Button basic inverted content="Login" />
-            <Button
-              basic
-              inverted
-              content="Sign Out"
-              style={{ marginLeft: "0.5em" }}
-            />
-          </Menu.Item>
+          {authenticate ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
