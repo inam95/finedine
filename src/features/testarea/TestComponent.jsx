caf4ddd0cf740762510d4f403ad240ad2461e08c
inam95 <mfihassan99@gmail.com>
2018-12-25 01:10:32 +0530

    reduxUtil added

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { incrementCounter, decrementCounter } from "./TestAction";
+import { Button } from "semantic-ui-react";
 
 const mapState = state => ({
   data: state.test.data
 });
 
+const actions = {
+  incrementCounter,
+  decrementCounter
+};
+
 class TestComponent extends Component {
   render() {
-    const { data } = this.props;
+    const { data, incrementCounter, decrementCounter } = this.props;
     return (
       <div>
         <h1>data={data}</h1>
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounter} color="red" content="Decrement" />
       </div>
     );
   }
 }
 
-export default connect(mapState)(TestComponent);
+export default connect(
+  mapState,
+  actions
+)(TestComponent);
