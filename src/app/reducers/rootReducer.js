fb714fec7438ce9f68d5d03ea5a6dfc0950f0e7d
inam95 <mfihassan99@gmail.com>
2018-12-25 23:08:20 +0530

    input components(util) added

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form'
+
 import testReducer from '../../features/testarea/testReducer';
 import restaurantReducer from '../../features/restaurant/restaurantReducer'
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: testReducer,
     restaurants: restaurantReducer
 });
