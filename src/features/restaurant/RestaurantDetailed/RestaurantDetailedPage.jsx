a44a2faca7a10e4c1d124cba591cc38c41f545d5
inam95 <mfihassan99@gmail.com>
2018-12-25 17:29:34 +0530

    redux added to detail page

--- src/features/restaurant/RestaurantDetailed/RestaurantDetailedPage.jsx
+++ src/features/restaurant/RestaurantDetailed/RestaurantDetailedPage.jsx
@@ -1,28 +1,45 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import RestaurantDetailedHeader from "./RestaurantDetailedHeader";
 
 import "./RestaurantDetailed.css";
 import RestaurantDetailedFooter from "./RestaurantDetailedFooter";
 
-const restaurant = {
-  id: "1",
-  name: "Dinemore",
-  phone: "085 3535 699",
-  address: {
-    city: "Kandy, SL",
-    street: "Dinemore, 15 George E De Silva Mawatha, Kandy"
-  },
-  tbr: true,
-  avg_cost: 1000,
-  likes: 150,
-  cuisines: ["American", "Fast Food"],
-  additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
-  description:
-    "Grabbing brunch or just stopping by for a drink - 'Dinemore' brings out an exciting, and effervescent vibe to the dining experience."
+const mapState = (state, ownProps) => {
+  const restaurantId = ownProps.match.params.id;
+
+  let restaurant = {};
+
+  if (restaurantId && state.restaurants.length > 0) {
+    restaurant = state.restaurants.filter(
+      restaurant => restaurant.id === restaurantId
+    )[0];
+  }
+
+  return {
+    restaurant
+  };
 };
 
-const RestaurantDetailedPage = () => {
+// const restaurant = {
+//   id: "1",
+//   name: "Dinemore",
+//   phone: "085 3535 699",
+//   address: {
+//     city: "Kandy, SL",
+//     street: "Dinemore, 15 George E De Silva Mawatha, Kandy"
+//   },
+//   tbr: true,
+//   avg_cost: 1000,
+//   likes: 150,
+//   cuisines: ["American", "Fast Food"],
+//   additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
+//   description:
+//     "Grabbing brunch or just stopping by for a drink - 'Dinemore' brings out an exciting, and effervescent vibe to the dining experience."
+// };
+
+const RestaurantDetailedPage = ({ restaurant }) => {
   return (
     <div className="detail-container">
       <RestaurantDetailedHeader restaurant={restaurant} />
@@ -31,4 +48,4 @@ const RestaurantDetailedPage = () => {
   );
 };
 
-export default RestaurantDetailedPage;
+export default connect(mapState)(RestaurantDetailedPage);
--- src/features/restaurant/restaurantReducer.jsx
+++ src/features/restaurant/restaurantReducer.jsx
@@ -5,28 +5,38 @@ const initialState = [
   {
     id: "1",
     name: "Dinemore",
+    phone: "085 3535 699",
     address: {
       city: "Kandy, SL",
       street: "Dinemore, 15 George E De Silva Mawatha, Kandy"
     },
+    tbr: true,
+    avg_cost: 1000,
+    likes: 150,
     rate: 4.2,
     votes: 147,
     cuisines: ["American", "Fast Food"],
+    additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
     description:
       "Grabbing brunch or just stopping by for a drink - 'Dinemore' brings out an exciting, and effervescent vibe to the dining experience."
   },
   {
     id: "2",
     name: "Open Rice",
+    phone: "085 3885 799",
     address: {
       city: "Kandy, SL",
       street: "Open Rice Sri Lanka, 701/A, Peradeniye Road, 20000"
     },
+    tbr: true,
+    avg_cost: 1000,
+    likes: 150,
     rate: 3.7,
     votes: 100,
     cuisines: ["Chinese", "Indian"],
+    additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
     description:
-      "Centrally located OpenRice is a Restaurant with specialty in multi delicious Cuisine created with the ingenuity and passion inspired by the fantastic flavors. Our variety menu includes Chinese, Indian, continental and western foods."
+      "Centrally located OpenRice is a Restaurant with specialty in multi delicious Cuisine created with the ingenuity and passion inspired by the fantastic flavors."
   }
 ];
 
