b4a8707f860e3f2701836258aafb2cbba4b8a1b7
inam95 <mfihassan99@gmail.com>
2018-12-26 12:22:38 +0530

    mok api added

---
+++ src/app/data/sampleData.js
@@ -0,0 +1,50 @@
+const sampleData = {
+    restaurants: [
+        {
+          id: "1",
+          name: "Dinemore",
+          phone: "085 3535 699",
+          address: {
+            city: "Kandy, SL",
+            street: "Dinemore, 15 George E De Silva Mawatha, Kandy"
+          },
+          tbr: true,
+          avg_cost: 1000,
+          placeLatLng: {
+            lat: 7.2875,
+            lng: 80.6231
+          },
+          likes: 150,
+          rate: 4.2,
+          votes: 147,
+          cuisines: ["American", "Fast Food"],
+          additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
+          description:
+            "Grabbing brunch or just stopping by for a drink - 'Dinemore' brings out an exciting, and effervescent vibe to the dining experience."
+        },
+        {
+          id: "2",
+          name: "Open Rice",
+          phone: "085 3885 799",
+          address: {
+            city: "Kandy, SL",
+            street: "Open Rice Sri Lanka, 701/A, Peradeniye Road, 20000"
+          },
+          tbr: true,
+          avg_cost: 1000,
+          placeLatLng: {
+            lat: 7.28,
+            lng: 80.6192
+          },
+          likes: 150,
+          rate: 3.7,
+          votes: 100,
+          cuisines: ["Chinese", "Indian"],
+          additionalInfo: ["Breakfast", "Brunch", "Private Dining Area Available"],
+          description:
+            "Centrally located OpenRice is a Restaurant with specialty in multi delicious Cuisine created with the ingenuity and passion inspired by the fantastic flavors."
+        }
+      ]
+}
+
+export default sampleData;
