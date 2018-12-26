11b984448a7a559984e4914363f9e433db9d55f2
inam95 <mfihassan99@gmail.com>
2018-12-26 12:18:11 +0530

    async actions added (features)

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -5,13 +5,15 @@ import testReducer from '../../features/testarea/testReducer';
 import restaurantReducer from '../../features/restaurant/restaurantReducer'
 import modalsReducer from '../../features/modals/modalReducer'
 import authReducer from '../../features/auth/authReducer'
+import asyncReducer from '../../features/async/asyncReducer'
 
 const rootReducer = combineReducers({
     form: FormReducer,
     test: testReducer,
     restaurants: restaurantReducer,
     modals: modalsReducer,
-    auth: authReducer
+    auth: authReducer,
+    async: asyncReducer
 });
 
 export default rootReducer;
